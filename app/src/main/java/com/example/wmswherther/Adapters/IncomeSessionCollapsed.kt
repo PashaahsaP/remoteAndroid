2f3b0589635efde687e8606c555a0ec8faf184c7
PashaahsaP <ppsairda@yandex.ru>
2025-10-27 04:54:58 +0700

    make some adding for IncomeSessionAdapter.kt

---
+++ app/src/main/java/com/example/wmswherther/Adapters/IncomeSessionCollapsed.kt
@@ -0,0 +1,17 @@
+package com.example.wmswherther.Adapters
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wmsRemote.R
+import com.example.wmswherther.Classes.IncomeItem
+
+class IncomeSessionCollapsed(itemView: View): RecyclerView.ViewHolder(itemView) {
+    val container: androidx.constraintlayout.widget.ConstraintLayout =
+        itemView as androidx.constraintlayout.widget.ConstraintLayout
+    val tvLeft: TextView = itemView.findViewById(R.id.tvName)
+
+    fun bind(item: IncomeItem) {
+        tvLeft.text = item.name
+    }
+}
---
+++ app/src/main/java/com/example/wmswherther/Adapters/IncomeSessionExpanded.kt
@@ -0,0 +1,18 @@
+package com.example.wmswherther.Adapters
+
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wmsRemote.R
+import com.example.wmswherther.Classes.IncomeItem
+
+class IncomeSessionExpanded (itemView: View): RecyclerView.ViewHolder(itemView) {
+    val container: androidx.constraintlayout.widget.ConstraintLayout =
+        itemView as androidx.constraintlayout.widget.ConstraintLayout
+    val tvLeft: TextView = itemView.findViewById(R.id.tvName)
+
+    fun bind(item: IncomeItem) {
+        tvLeft.text = item.name
+    }
+}
